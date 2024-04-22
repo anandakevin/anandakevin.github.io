5e34663026bcb8332c985d412ace5131a7b10cf4
Ananda Kevin Refaldo Sariputra <ananda_sariputra@bca.co.id>
2024-04-22 18:10:26 +0700

    feat: Added Proper Responsive Navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import '../scss/styles.scss';
+import '../scss/font-styles.scss';
+import '../scss/figures.scss';
 
 import * as bootstrap from 'bootstrap';
 
@@ -16,3 +18,19 @@ import * as bootstrap from 'bootstrap';
 // server.listen(port, hostname, () => {
 //     console.log(`Server running at http://${hostname}:${port}/`)
 // })
+
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+	isNavbarExpanded = !isNavbarExpanded;
+	navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+// navbarMenu.addEventListener('click', toggleNavbarVisibility);
